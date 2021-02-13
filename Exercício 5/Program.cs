8f167a2fb837ed01beb191a1830a5bd8c76f460a
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-13 18:45:45 -0300

    Add files via upload

---
+++ Exercício 5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+namespace Exercício_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numeros;
+
+            int n = int.Parse(Console.ReadLine());
+
+            numeros = new double[n];
+            double soma = 0;
+            int contador = 0;
+            string[] vetor = Console.ReadLine().Split(' ');
+            for (int i = 0; i < n; i++)
+            {
+                numeros[i] = int.Parse(vetor[i], CultureInfo.InvariantCulture);
+                if(numeros[i]%2==0)
+                {
+                    soma = soma + numeros[i];
+                    contador++;
+                }
+                
+            }
+            double media = 0;
+
+            media = soma / contador;
+            Console.WriteLine(media.ToString("F1",CultureInfo.InvariantCulture));
+        }
+    }
+}
